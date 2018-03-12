4a0dfa8d8cb3c7b7ac7725cafa20028ed6eb1e82
Ben_Alaya_Achraf <Ben_Alaya_Achraf@outlook.com>
2018-03-12 18:13:21 +0100

    firstpush

---
+++ src/app/user/sign-up/sign-up.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import{NgModule} from '@angular/core'
+
+import { NgForm } from '@angular/forms';
+import { Validators } from '@angular/forms';
+
+ import { ToastrService } from 'ngx-toastr';
+import { UserService } from '../../shared/user.service';
+import { User } from '../../shared/user.model';
+ 
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent implements OnInit {
+user:User;
+emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+  constructor(private UserService : UserService, private toastr: ToastrService) { }
+
+
+
+  ngOnInit() {
+    this.resetForm();
+  }
+
+
+  resetForm(form? :NgForm )
+  {
+if(form!=null)
+form.reset();
+this.user={
+
+UserName:'',
+Password:'',
+Email:'',
+FirstName:'',
+LastName:''
+
+}
+  }
+  
+  OnSubmit(form: NgForm) {
+    this.UserService.registerUser(form.value)
+      .subscribe((data: any) => {
+        if (data.Succeeded == true) {
+          this.resetForm(form);
+           this.toastr.success('User registration successful');
+        }
+      else
+          this.toastr.error(data.Errors[0]);
+      });
+  }
+}
